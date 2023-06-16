7a875acefa21e5ee7eb848516da89feb071971ba
Sahadathossain <sahadathossain164109@gmail.com>
2023-06-16 21:21:14 +0600

    added update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,8 +3,12 @@ import { Helmet } from "react-helmet";
 import TopInstructors from "../Top Instructors/TopInstructors";
 import Testimonial from "../Testimonial/Testimonial";
 import ContactForm from "../ContactForm/ContactForm";
+import Hero from "../Hero/Hero";
 
-// import Section from "../Section/Section";
+
+
+
+
 
 
 
@@ -16,7 +20,8 @@ const Home = () => {
             <Helmet>
                 <title> SA | Home</title>
             </Helmet>
-           
+        
+        <Hero></Hero>
             <TopInstructors></TopInstructors>
             <Testimonial></Testimonial>
             <ContactForm></ContactForm>
